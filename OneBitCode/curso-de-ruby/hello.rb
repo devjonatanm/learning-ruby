0b752530a7ff09db6bc0d6189dea689319ccae22
Jonatan Marques <102691032+devjonatanm@users.noreply.github.com>
2025-04-10 17:10:57 -0300

    Create hello.rb

---
+++ OneBitCode/curso-de-ruby/hello.rb
@@ -0,0 +1,12 @@
+# Comandos de Saída de Dados
+
+print "Hello World\n"
+print "Olá mundo\n"
+puts "Hello World"
+puts "Olá mundo"
+
+=begin
+Estou aprendendo
+a Linguagem Ruby
+na One Bit Code
+=end
