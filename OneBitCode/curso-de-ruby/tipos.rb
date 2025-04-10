649684ae220838531877eb37b2f3d05aadbb85c5
Jonatan Marques <102691032+devjonatanm@users.noreply.github.com>
2025-04-10 17:12:52 -0300

    Create tipos.rb

---
+++ OneBitCode/curso-de-ruby/tipos.rb
@@ -0,0 +1,13 @@
+# Dados do Filme
+name = "Avatar"
+yearLaunch = 2022
+price = 100.00
+planIncluded = false
+
+puts name, yearLaunch
+
+# Identificando os tipos de dados
+puts name.class
+puts yearLaunch.class
+puts price.class
+puts planIncluded.class
