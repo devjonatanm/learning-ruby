ce3d87c78c8a836255ba05a9ea7bf10174e246a9
Jonatan Marques <102691032+devjonatanm@users.noreply.github.com>
2025-04-12 21:27:49 -0300

    Create strings.rb

---
+++ OneBitCode/curso-de-ruby/strings.rb
@@ -0,0 +1,18 @@
+# Dados do Filme
+name = "De Volta para o Futuro"
+name2 = "de volta para o futuro"
+
+# Ruby é case sensitive
+puts name == name2
+
+description = "O filme trata de uma viagem no tempo"
+description2 = <<Text
+O filme #{name} é um clássico
+de 1985 que trata sobre viagem no tempo
+Text
+
+puts description2
+
+# Tamanho da String
+puts description.length
+puts description.size
